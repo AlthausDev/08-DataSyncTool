ce492bb860f4df8eb8e5750ee8c1003d087a1408
AlthausDev <samuelalthaus@gmail.com>
2023-11-13 19:26:12 +0100

    feat: Mejorar el script para gestionar un número variable de acciones y descripciones

--- Bridge.bat
+++ Bridge.bat
@@ -2,8 +2,8 @@
 echo Ejecutando Script 1
 
 rem Define la acción
-set Accion= **AccionX:**
-set Descripcion= Eliminación y copia de archivos
+:: set Accion= **AccionX:**
+:: set Descripcion= Eliminación y copia de archivos
 
 
 rem Llama al Segundo Script (changelog-generator.bat)
--- changelog-generator.bat
+++ changelog-generator.bat
@@ -1,6 +1,11 @@
 @echo off
 
-set LogDirectorio=D:\Log
+rem Obtener la ruta del directorio donde se encuentra el script
+set "ScriptDirectorio=%~dp0"
+
+rem Establecer la carpeta "log" en el mismo directorio que el script
+set "LogDirectorio=%ScriptDirectorio%log"
+
 set LogArchivo=CHANGELOG.md
 set TempArchivo=TEMPCHANGELOG.md
 
@@ -10,12 +15,35 @@ if not exist "%LogDirectorio%" mkdir "%LogDirectorio%"
 rem Obtener la fecha y la hora actual
 set FechaHora=%DATE% %TIME%
 
+rem Definir una matriz de acciones y descripciones
+set "Acciones[1]=%Accion1%"
+set "Acciones[2]=%Accion2%"
+set "Acciones[3]=%Accion3%"
+set "Acciones[4]=%Accion4%"
+set "Acciones[5]=%Accion5%"
+
+set "Descripciones[1]=%Descripcion1%"
+set "Descripciones[2]=%Descripcion2%"
+set "Descripciones[3]=%Descripcion3%"
+set "Descripciones[4]=%Descripcion4%"
+set "Descripciones[5]=%Descripcion5%"
+
 rem Crear o agregar al archivo temporal con formato de CHANGELOG
 (
     echo ## Cambios Recientes
     echo.
     echo ### %FechaHora%
-    echo - %Accion% %Descripcion%
+
+    rem Bucle para acciones y descripciones
+    for /L %%i in (1,1,5) do (
+        rem Verificar si ambas variables están definidas
+        if defined Acciones[%%i] (
+            rem Agregar acción y descripción al archivo temporal
+            call echo - %%Acciones[%%i]%% %%Descripciones[%%i]%%
+            echo.
+        )
+    )
+
     echo.
     echo.
 ) > "%LogDirectorio%\%TempArchivo%"
@@ -27,5 +55,4 @@ rem Sobrescribir CHANGELOG.md con el contenido del archivo temporal
 move /y "%LogDirectorio%\%TempArchivo%" "%LogDirectorio%\%LogArchivo%"
 
 echo Registro completado.
-
 
